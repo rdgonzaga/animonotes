fadfbd53f7309ef3934c5557566126612c1571c5
rdgonzaga <rainerdgonzaga@gmail.com>
2026-02-11 17:54:29 +0800

    chore: remove test suites and prune dev dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -43,5 +40,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
