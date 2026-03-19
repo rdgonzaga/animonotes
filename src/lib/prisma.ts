6649ff6c8a58b6b69d32d46b76b31754862cb7b9
rdgonzaga <rainerdgonzaga@gmail.com>
2026-03-19 16:22:10 +0800

    refactor(prisma): fix prisma not seeding properly

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,9 +10,21 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const runtimeDatabaseUrl =
+  process.env.NODE_ENV === 'development'
+    ? process.env.DIRECT_URL || process.env.DATABASE_URL
+    : process.env.DATABASE_URL;
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasources: runtimeDatabaseUrl
+      ? {
+          db: {
+            url: runtimeDatabaseUrl,
+          },
+        }
+      : undefined,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
